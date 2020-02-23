929d0e8dce5df757ff6cb8bb3895ee2164fa0902
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-23 16:54:40 +0100

    first commit

---
+++ src/main/java/com/safa/country/soap/ws/CountryDetailsSoapsWsApplication.java
@@ -0,0 +1,13 @@
+package com.safa.country.soap.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryDetailsSoapsWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryDetailsSoapsWsApplication.class, args);
+	}
+
+}
