f92ebcb8437bfcaa58aa952d76ac31bf8a7bc658
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-23 17:40:28 +0100

    create xsd file and generate countrydetails package

---
+++ src/main/java/com/safa/country/soap/ws/countrydetails/Country.java
@@ -0,0 +1,144 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
+// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2020.02.23 à 05:35:18 PM WAT 
+//
+
+
+package com.safa.country.soap.ws.countrydetails;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour country complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="country">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="population" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="capital" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="currency" type="{http://www.safa.com/country/soap/ws/countryDetails}currency"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "country", propOrder = {
+    "name",
+    "population",
+    "capital",
+    "currency"
+})
+public class Country {
+
+    @XmlElement(required = true)
+    protected String name;
+    protected int population;
+    @XmlElement(required = true)
+    protected String capital;
+    @XmlElement(required = true)
+    protected Currency currency;
+
+    /**
+     * Obtient la valeur de la propriété name.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Définit la valeur de la propriété name.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété population.
+     * 
+     */
+    public int getPopulation() {
+        return population;
+    }
+
+    /**
+     * Définit la valeur de la propriété population.
+     * 
+     */
+    public void setPopulation(int value) {
+        this.population = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété capital.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCapital() {
+        return capital;
+    }
+
+    /**
+     * Définit la valeur de la propriété capital.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCapital(String value) {
+        this.capital = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété currency.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Currency }
+     *     
+     */
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    /**
+     * Définit la valeur de la propriété currency.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Currency }
+     *     
+     */
+    public void setCurrency(Currency value) {
+        this.currency = value;
+    }
+
+}
---
+++ src/main/java/com/safa/country/soap/ws/countrydetails/Currency.java
@@ -0,0 +1,47 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
+// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2020.02.23 à 05:35:18 PM WAT 
+//
+
+
+package com.safa.country.soap.ws.countrydetails;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour currency.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="currency">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="GBP"/>
+ *     &lt;enumeration value="EUR"/>
+ *     &lt;enumeration value="PLN"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "currency")
+@XmlEnum
+public enum Currency {
+
+    GBP,
+    EUR,
+    PLN;
+
+    public String value() {
+        return name();
+    }
+
+    public static Currency fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
